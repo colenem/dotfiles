290823f3dae13eafc9ea0e0e1cbe8d6e0a419e6d
Colene McFarlane <cmcfarlane22@gmail.com>
2020-07-23 05:12:36 -0400

    Added zypper aliases.

---
+++ zsh/v2/zypper.zsh
@@ -0,0 +1,5 @@
+alias zup="sudo zypper up $1"
+alias zdup="sudo zypper dup $1"
+alias znfo="zypper info --provides --requires --recommends --suggests $1"
+alias zrch="zypper search $1"
+alias zrf='sudo zypper refresh'
