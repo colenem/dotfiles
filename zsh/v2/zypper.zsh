9d5eaf5ef40af5a88453dfa393f63f9c577d312b
Colene McFarlane <cmcfarlane22@gmail.com>
2021-10-12 00:26:41 -0400

    Update zsh aliases.

--- zsh/v2/zypper.zsh
+++ zsh/v2/zypper.zsh
@@ -1,7 +1,8 @@
 alias zup="sudo zypper up $1"
 alias zdup="sudo zypper dup $1"
-alias znfo="zypper info --provides --requires --recommends --suggests $1"
-alias znstall="sudo zypper install $1"
+alias zlup="sudo zypper list-updates"
+alias znfo="zypper info --provides --requires --recommends --suggests --conflicts $1"
+alias znst="sudo zypper install $1"
 alias zrch="zypper search $1"
 alias zrf='sudo zypper refresh'
-alias zrm='sudo zypper remove $1'
+alias zrm="sudo zypper remove $1"
